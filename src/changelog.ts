b7687985954bf41f2fc0c9f0a72247e15bce7e89
cg-cnu <sreenivas9alapati@gmail.com>
2017-11-15 23:06:39 +0530

    skip workspace sel on one ws

--- src/changelog.ts
+++ src/changelog.ts
@@ -2,31 +2,32 @@
 import * as vscode from 'vscode';
 import { workspace } from 'vscode';
 import { join } from 'path';
-import { existsSync, openSync, closeSync} from 'fs';
+import { existsSync, openSync, closeSync } from 'fs';
 
 export function activate(context: vscode.ExtensionContext) {
-
+    var workspaceObj: string = ''
     let changelog = vscode.commands.registerCommand('changelog.create', () => {
         const workspaces = vscode.workspace.workspaceFolders;
-        const workspaceNames = workspaces.map(workspace => workspace.name);
-        // TODO: created by admin @ 2017-11-13 14:35:48
-        // if only one workspace choose directly
-        console.log('workspaceNames', workspaceNames)
-        vscode.window.showQuickPick(workspaceNames)
-            .then(workspaceName => {
-                // get the workspace object 
-                const workspace = workspaces.find(
-                    workspace => {
-                        return workspace.name === workspaceName;
-                    }
-                )
-                console.log('workspace', workspace)
-                // create CHANGELOG.md if not exists
-                const changeLogFile = join(workspace.uri.fsPath, 'CHANGELOG.md')
-                if (!existsSync(changeLogFile)) {
-                    closeSync(openSync(changeLogFile, 'a'));
-                }
-            })
+        if (workspaces.length !== 1) {
+            const workspaceNames = workspaces.map(workspace => workspace.name);
+            vscode.window.showQuickPick(workspaceNames)
+                .then(workspaceName => {
+                    // get the workspace object 
+                    const workspaceObj = workspaces.find(
+                        workspace => {
+                            return workspace.name === workspaceName;
+                        }
+                    )
+                    // console.log('workspace', workspace)
+                })
+        } else {
+            const workspaceObj = workspaces[0]
+        }
+        // create CHANGELOG.md if not exists
+        const changeLogFile = join(workspaceObj.uri.fsPath, 'CHANGELOG.md')
+        if (!existsSync(changeLogFile)) {
+            closeSync(openSync(changeLogFile, 'a'));
+        }
         // ask for the release version
         // if exists increment the existing one ?
 
@@ -36,7 +37,7 @@ export function activate(context: vscode.ExtensionContext) {
             'Removed: for now removed features',
             'Fixed: for any bugs',
             'Security: in case of vulnerabilities.']
-            
+
         // ask for the type of changes...
         vscode.window.showQuickPick(changeTypes)
             .then(changeType => {
