a98e38e72845063aff5208d1d070bf9d184571c1
cg-cnu <sreenivas9alapati@gmail.com>
2017-11-19 23:11:18 +0530

    update

--- src/changelog.ts
+++ src/changelog.ts
@@ -4,35 +4,50 @@ import { workspace } from 'vscode';
 import { join } from 'path';
 import { existsSync, openSync, closeSync } from 'fs';
 
-const changeTypes = [
-    'Changed: for changes in existing functionality',
-    'Deprecated: for soon-to-be removed features.',
-    'Removed: for now removed features',
-    'Fixed: for any bugs',
-    'Security: in case of vulnerabilities.']
-
-// ask for the type of changes...
-vscode.window.showQuickPick(changeTypes)
-    .then(changeType => {
-        if (!changeType) {
-            return;
-        }
-        vscode.window.showInputBox({
-            ignoreFocusOut: true,
-            placeHolder: 'Description here...',
-            prompt: 'Enter the descriptions of the change'
-        }).then(description => {
-            if (!description) {
-                return;
-            }
-            console.log('changeType', changeType)
-            console.log('description', description)
-            // TODO: created by admin @ 2017-11-13 23:26:21
-            // write to the file
-        })
-    })
 
 export function activate(context: vscode.ExtensionContext) {
+
+    // add new version to changelog
+    let addVersion = vscode.commands.registerCommand('changelog.addVersion', () => {
+        // show the last version
+        // Ask them to increment the version
+    })
+    context.subscriptions.push(addVersion);
+    
+    // add changes to the changelog
+    let addChange = vscode.commands.registerCommand('changelog.addChange', () => {
+
+        const changeTypes = [
+            'Changed: for changes in existing functionality',
+            'Deprecated: for soon-to-be removed features.',
+            'Removed: for now removed features',
+            'Fixed: for any bugs',
+            'Security: in case of vulnerabilities.']
+
+        // ask for the type of changes...
+        vscode.window.showQuickPick(changeTypes)
+            .then(changeType => {
+                if (!changeType) {
+                    return;
+                }
+                vscode.window.showInputBox({
+                    ignoreFocusOut: true,
+                    placeHolder: 'Description here...',
+                    prompt: 'Enter the descriptions of the change'
+                }).then(description => {
+                    if (!description) {
+                        return;
+                    }
+                    console.log('changeType', changeType)
+                    console.log('description', description)
+                    // TODO: created by admin @ 2017-11-13 23:26:21
+                    // write to the file
+                })
+            })
+        })
+        context.subscriptions.push(addChange);
+
+    // Create changelog if don't exist
     let create = vscode.commands.registerCommand('changelog.create', () => {
         var workspaceObj;
         const workspaces = vscode.workspace.workspaceFolders;
@@ -55,9 +70,8 @@ export function activate(context: vscode.ExtensionContext) {
         if (!existsSync(changeLogFile)) {
             closeSync(openSync(changeLogFile, 'a'));
         }
-        // ask for the release version
-        // if exists increment the existing one ?
-
+        // TODO: created by admin @ 2017-11-19 23:10:37
+        // Write the contents to the file
     });
     context.subscriptions.push(create);
 }
